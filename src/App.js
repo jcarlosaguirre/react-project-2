a416a95a1c623b543ea1ea7eaf654ca7f979512d
Juan Carlos Aguirre <juancarlos.aguirrecortes@gmail.com>
2022-02-20 20:21:58 +0100

    broken structure into components and styled menu items

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 
+const HatsPage = () => (
+    <div>
+        <Link to='/hats/3'>Hats 3</Link>
+        <h1>Hats page</h1>
+    </div>
+)
+
+const HatsDetailPage = (props) => {
+
+    let params = props.match.params
+
+    return (
+        <div>
+            <h1>Hats page: { params.hatId }</h1>
+        </div>
+    );
+}
+
+
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+        <Link to='/hats'>Hats</Link>
+        <Switch>
+            <Route exact path='/' component={HomePage}/>
+            <Route exact path='/hats' component={HatsPage}/>
+            <Route path='/hats/:hatId' component={HatsDetailPage}/>
+        </Switch>
     </div>
   );
 }
